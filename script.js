204d8713c4d8558dc058b1db299cc3e407fe4571
bhupeshvision01-gif <bhupeshvision01@gmail.com>
2026-03-26 01:25:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,47 @@
-function ytIncome(){
+function youtubeIncome(){
 
 let views=parseFloat(document.getElementById("views").value);
 let cpm=parseFloat(document.getElementById("cpm").value);
 
 let income=(views/1000)*cpm;
 
-document.getElementById("ytResult").innerText="$"+income.toFixed(2);
+document.getElementById("result").innerText="$"+income.toFixed(2);
 
 }
 
 
-
-function roiCalc(){
+function roiCalculator(){
 
 let gain=parseFloat(document.getElementById("gain").value);
 let cost=parseFloat(document.getElementById("cost").value);
 
 let roi=((gain-cost)/cost)*100;
 
-document.getElementById("roiResult").innerText=roi.toFixed(2)+"%";
+document.getElementById("result").innerText=roi.toFixed(2)+"%";
 
 }
 
 
-
-function emiCalc(){
+function emiCalculator(){
 
 let loan=parseFloat(document.getElementById("loan").value);
 let rate=parseFloat(document.getElementById("rate").value)/1200;
-let years=parseFloat(document.getElementById("years").value)*12;
+let months=parseFloat(document.getElementById("months").value);
 
-let emi=loan*rate*Math.pow(1+rate,years)/(Math.pow(1+rate,years)-1);
+let emi=loan*rate*Math.pow(1+rate,months)/(Math.pow(1+rate,months)-1);
 
-document.getElementById("emiResult").innerText=emi.toFixed(2);
+document.getElementById("result").innerText=emi.toFixed(2);
 
 }
 
 
-
-function bmiCalc(){
+function bmiCalculator(){
 
 let weight=parseFloat(document.getElementById("weight").value);
 let height=parseFloat(document.getElementById("height").value)/100;
 
 let bmi=weight/(height*height);
 
-document.getElementById("bmiResult").innerText=bmi.toFixed(2);
+document.getElementById("result").innerText=bmi.toFixed(2);
 
 }
