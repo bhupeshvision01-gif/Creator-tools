2f34ec4dd3e5c410eafd9e7a9493b00cbdc85bc7
bhupeshvision01-gif <bhupeshvision01@gmail.com>
2026-04-18 21:38:20 +0530

    Create BlogIndex.tsx

---
+++ src/blog/BlogIndex.tsx
@@ -0,0 +1,6 @@
+<ul>
+  <li><Link to="/blog/post1">How Creators Can Calculate ROI</Link></li>
+  <li><Link to="/blog/post2">AdSense Approval Guide for New Sites</Link></li>
+  <li><Link to="/blog/post3">SEO Tips for Creator Websites</Link></li>
+  <li><Link to="/blog/tax-hacks">Tax Hacks for Digital Creators in 2026</Link></li>
+</ul>
